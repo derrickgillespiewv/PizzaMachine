ab6a315483363709babd2763eb5224c9972f28f8
Derrick Gillespie <derrickgillespiewv@gmail.com>
2017-06-30 09:48:11 -0400

    Update 2

--- pizza.rb
+++ pizza.rb
@@ -28,29 +28,66 @@ def finish
 end
 
 
+
 def order
-puts "How many Pizza would you like?"
-order = gets.to_i
-count =0
-# puts "Do you wnat some extra cheese, meat, or both? Type cheese, meat, or both."
-# extra =gets.chomp
-# case extra()
 
-# when cheese 
-# order.times do 
-# count+=1 
-# print "Pizza #{count} is made with #{crust} Crust, #{cheese} Cheese and #{cheese} Cheese, #{sauce} Sauce, #{pepper}, #{veggie}, #{animal}, and a #{finish} finish\n"
+	puts "How many Pizza would you like?"
+	order = gets.to_i
+	count =0
+	order.times do 
+	count+=1
+	puts "Do you wnat some extra cheese, meat, or both? Type (c)heese, (m)eat, (b)oth, or (n)one."
+	extra = gets.chomp
+
+	case extra
+
+	when "c"
+		count =0
+	order.times do 
+	count+=1
+		print "Pizza #{count} is made with #{crust} Crust, #{cheese} Cheese and #{cheese} Cheese, #{sauce} Sauce, #{pepper}, #{veggie}, #{animal}, and a #{finish} finish\n"
+		money = (count*10)*1.06
+		print "The total price $#{money}\n"
+	end
+	exit
+
+	when "m"
+		count =0
+	order.times do 
+	count+=1
+		print "Pizza #{count} is made with #{crust} Crust, #{cheese} Cheese, #{sauce} Sauce, #{pepper}, #{veggie}, #{animal} and #{animal} meat, and a #{finish} finish\n"
+		money = (count*11)*1.06
+		print "The total price $#{money}\n"
+	end
+	exit
+
+	when "b"
+		count =0
+	order.times do 
+	count+=1
+		print "Pizza #{count} is made with #{crust} Crust, #{cheese} and #{cheese} Cheese, #{sauce} Sauce, #{pepper}, #{veggie}, #{animal} and #{animal} meat, and a #{finish} finish\n"
+		money = (count*12)*1.06
+		print "The total price $#{money}\n"
+	end
+
+	exit
+
+	when "n"
+		count =0
+	order.times do 
+	count+=1
+		print "Pizza #{count} is made with #{crust} Crust, #{cheese} Cheese, #{sauce} Sauce, #{pepper}, #{veggie}, #{animal}, and a #{finish} finish\n"
+		money = (count*8)*1.06
+		print "The total price $#{money}\n"
+	end
+	exit
 
 
-# when meat
-# when both
-order.times do 
-count+=1 
-print "Pizza #{count} is made with #{crust} Crust, #{cheese} Cheese, #{sauce} Sauce, #{pepper}, #{veggie}, #{animal}, and a #{finish} finish\n"
+	else 
+		print "this is not a valid input. Try Again"
+	end
+	exit
+	end
 
 end
-money = count*8
-print "The total price $#{money}"
-end
-
 order
